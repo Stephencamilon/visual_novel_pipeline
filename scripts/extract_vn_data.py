ccec4c92363dbd2b2ce24ccfc3ca5d2f43714875
Stephencamilon <113228328+Stephencamilon@users.noreply.github.com>
2025-10-10 16:02:50 +0800

    added gitignore file.

--- scripts/extract_vn_data.py
+++ scripts/extract_vn_data.py
@@ -6,35 +6,40 @@ def extract_vn_data():
     # Ensures output folder exists
     os.makedirs("data/raw", exist_ok=True)
 
-    # VNDB API endpoint
     url = "https://api.vndb.org/kana/vn"
-
-    # Request body - asking for the first 100 released visual novels
-    payload = {
-        # "filters": ["released", "=", True],
-        "fields": ("id, title, aliases, released, rating, developers{id,name}, length, platforms, image{id, url}, languages, description"),
-        "results": 100,
-        "page": 1
-    }
-
-    print("Fetching data from VNDB API...")
-    response = requests.post(url, json=payload, headers={"Content-Type": "application/json"})
-
-    # response status
-    if response.status_code == 200:
-        data = response.json()
-        os.makedirs("data/raw", exist_ok=True)
-
-        with open("data/raw/vn_data.json", "w", encoding="utf-8") as f:
-            json.dump(data, f, indent=4, ensure_ascii=False)
-
-        print(f"Saved {len(data.get('results', []))} visual novels to data/raw/vn_data.json")
+    all_results = []
+    page = 1
+    while True:
+        payload = {
+            # "filters": ["released", "=", True],
+            "fields": ("id, title, aliases, released, rating, developers{id,name}, length, platforms, image{id, url}, languages, description"),
+            "results": 100,
+            "page": page
+        }
+        print(f"Fetching page {page} from VNDB API...")
+        response = requests.post(url, json=payload, headers={"Content-Type": "application/json"})
+        if response.status_code == 200:
+            data = response.json()
+            results = data.get('results', [])
+            if not results:
+                print("No more results found.")
+                break
+            all_results.extend(results)
+            print(f"Fetched {len(results)} visual novels from page {page}.")
+            if len(results) < 100:
+                print("Last page reached.")
+                break
+            page += 1
+        else:
+            print(f"Failed to fetch data. Status code: {response.status_code}")
+            print(response.text)
+            break
+    with open("data/raw/vn_data.json", "w", encoding="utf-8") as f:
+        json.dump({"results": all_results}, f, indent=4, ensure_ascii=False)
+    print(f"Saved {len(all_results)} visual novels to data/raw/vn_data.json")
+    print("Response JSON", data)
    
-    else:
-        print(f"Failed to fetch data. Status code: {response.status_code}")
-        print(response.text)
    
-    print("Response JSON", response.json())
 
 if __name__ == "__main__":
     extract_vn_data()
